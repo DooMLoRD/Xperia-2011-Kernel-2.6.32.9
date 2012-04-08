ff4d82d7befe633c360b4c67b360cc4731c5bae0
DooMLoRD <metoo.mememe@gmail.com>
2012-04-09 01:50:04 +0530

    merged new 4.0.2.A.0.58 FW kernel sources

--- kernel/arch/arm/mach-msm/qdsp5v2/audio_acdb.c
+++ kernel/arch/arm/mach-msm/qdsp5v2/audio_acdb.c
@@ -204,7 +204,8 @@ static u8 check_device_info_already_present(
 		/*checking for cache node status if it is not filled then the
 		acdb values are not cleaned from node so update node status
 		with acdb value filled*/
-		if (acdb_cache_free_node->node_status != ACDB_VALUES_FILLED) {
+		if ((acdb_cache_free_node->node_status != ACDB_VALUES_FILLED) &&
+			((audcal_info.dev_type & RX_DEVICE) == 1)) {
 			MM_DBG("device was released earlier\n");
 			acdb_cache_free_node->node_status = ACDB_VALUES_FILLED;
 			acdb_data.acdb_state |= CAL_DATA_READY;
@@ -1431,13 +1432,10 @@ static u32 free_acdb_cache_node(union auddev_evt_data *evt)
 		acdb_cache_tx[session_id].
 			node_status = ACDB_VALUES_NOT_FILLED;
 	} else {
-		if (--(acdb_cache_rx[evt->audcal_info.dev_id].stream_id) <= 0) {
 			MM_DBG("freeing rx cache node %d\n",
 						evt->audcal_info.dev_id);
 			acdb_cache_rx[evt->audcal_info.dev_id].
 				node_status = ACDB_VALUES_NOT_FILLED;
-			acdb_cache_rx[evt->audcal_info.dev_id].stream_id = 0;
-		}
 	}
 	return 0;
 }
@@ -1567,8 +1565,13 @@ static void audpp_cb(void *private, u32 id, u16 *msg)
 		goto done;
 
 	if (msg[0] == AUDPP_MSG_ENA_DIS) {
-		acdb_data.acdb_state &= ~AUDPP_READY;
-		MM_DBG("AUDPP_MSG_ENA_DIS\n");
+		if (--acdb_cache_rx[acdb_data.\
+				device_info->dev_id].stream_id <= 0) {
+			acdb_data.acdb_state &= ~AUDPP_READY;
+			acdb_cache_rx[acdb_data.device_info->dev_id]\
+					.stream_id = 0;
+			MM_DBG("AUDPP_MSG_ENA_DIS\n");
+		}
 		goto done;
 	}
 
